4f5a1fe6c39346d3f6bc68d0e713f92562643e3f
andrew <andrew841018@gmail.com>
2026-03-07 21:49:21 +0800

    1. 用pipeline串起：爬蟲＋資料清洗環節 2. web_scraping+analysis都只放function不實際上做事，真的執行是靠pipeline來呼叫

--- analysis.py
+++ analysis.py
@@ -1,6 +1,5 @@
 import pandas as pd 
 import re
-from tqdm import tqdm
 import sqlite3
 from sentiment import calculate_sentiment
 def numberic_push_count(push_count):
@@ -12,7 +11,9 @@ def numberic_push_count(push_count):
         return int(push_count.strip("X"))*-10
     else:
         return int(push_count)
-def clean_article_info(conn,cursor):
+def clean_article_info():
+    conn=sqlite3.connect("ptt_stock.db")
+    cursor=conn.cursor()
     df=pd.read_sql_query("SELECT * FROM ptt_stock_article_info",conn)
     pd.set_option('display.max_colwidth', None)
     df['Article_Sentiment_Score']=df.progress_apply(lambda row: calculate_sentiment(str(row['Title']))*2+calculate_sentiment(str(row['Content'])),axis=1)
@@ -27,7 +28,11 @@ def clean_article_info(conn,cursor):
     df['Article_Sentiment_Score']=df['Article_Sentiment_Score'].astype(int)
     record_list=df[['Published_Time','Push_count','Article_Sentiment_Score','Article_id']].values.tolist()
     cursor.executemany("UPDATE ptt_stock_article_info SET Published_Time=?, Push_count=?, Article_Sentiment_Score=? WHERE Article_id=?",record_list)
-def clean_comment_info(conn,cursor):
+    conn.commit()
+    conn.close()
+def clean_comment_info():
+    conn=sqlite3.connect("ptt_stock.db")
+    cursor=conn.cursor()
     df=pd.read_sql_query("SELECT * FROM ptt_stock_comment_info",conn)
     df['Message']=df['Message'].str.lstrip(":")
     df['Comment_Sentiment_Score']=df['Message'].progress_apply(calculate_sentiment)
@@ -35,21 +40,5 @@ def clean_comment_info(conn,cursor):
     # store back to database
     record_list=df[['Comment_Sentiment_Score','Comment_id']].values.tolist()
     cursor.executemany("UPDATE ptt_stock_comment_info SET Comment_Sentiment_Score=? WHERE Comment_id=?",record_list)
-if __name__=="__main__":
-    conn=sqlite3.connect('ptt_stock.db')
-    cursor=conn.cursor()
-    tqdm.pandas()
-    # create new column
-    for sql in ["ALTER TABLE ptt_stock_article_info ADD COLUMN Article_Sentiment_Score INTEGER",
-                "ALTER TABLE ptt_stock_comment_info ADD COLUMN Comment_Sentiment_Score INTEGER",
-                "ALTER TABLE ptt_stock_article_info ADD COLUMN Published_Time INTEGER"]:
-        try:
-            cursor.execute(sql)
-            conn.commit()
-        except sqlite3.OperationalError:
-            print(f"Column {sql} already exist")
-    clean_article_info(conn,cursor)
-    clean_comment_info(conn,cursor)
     conn.commit()
     conn.close()
-    print("Data cleaning completed")
