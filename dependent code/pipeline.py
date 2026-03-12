2dd42a8f06f3da24040712dcb577ea0fcff48272
andrew <andrew841018@gmail.com>
2026-03-12 16:31:24 +0800

    1. 將類似code用資料夾區分 2. 新增自動測試api 的code 3. 完成一些基礎的api，將可能需要的地方讓使用者可以彈性調整

--- dependent code/pipeline.py
+++ dependent code/pipeline.py
@@ -10,13 +10,10 @@ from analysis import clean_article_info,clean_comment_info
 logging.basicConfig(level=logging.INFO,format="%(asctime)s - %(levelname)s - %(message)s")#initialize logging
 ## global variable
 MAX_RETRY=5
-def web_scraping():
+def web_scraping(): 
     #init    
     url = "https://www.ptt.cc/bbs/stock/index.html" #初始url
     conn=sqlite3.connect("ptt_stock.db")
-    ## delte content before insert
-    ## note that need to delete comment_info first because of the foreign key constraint
-    conn.commit()
     for i in tqdm(range(900),desc="爬蟲頁數"):#爬蟲頁數
         retry = 0
         while retry < MAX_RETRY:
--- dependent code/web_scraping.py
+++ dependent code/web_scraping.py
@@ -2,7 +2,6 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-import logging
 import sqlite3
 from datetime import datetime
 # functions
